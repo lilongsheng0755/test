e51874d571683b96052028c59577f977c2cc5471
lilongsheng <1226364644@qq.com>
2018-06-21 22:03:53 +0800

    update dbtest

--- dbtest.php
+++ dbtest.php
@@ -11,7 +11,7 @@ $mysqli = new mysqli('127.0.0.1', 'test', 'test', 'test');
 $setNum = 1000000; //设置插入数据行数
 $ctime = time();
 $mc = microtime(true);
-for ($i = 2; $i <= $setNum; $i++) {
+for ($i = 1; $i <= $setNum; $i++) {
     $sql = "INSERT INTO `baseinfo` SET `uid`={$i}, `realname`='user_{$i}', `idcard`='44092114566{$i}', `nickname`='user_{$i}',";
     $sql .= "`icon`='http://192.168.1.111:81/dokuwiki/lib/tpl/dokuwiki/images/apple-touch-icon.png', `phone`='14718052215',`area`='中国', `register`={$ctime}, `lastlogin`={$ctime}";
     $mysqli->query($sql);
