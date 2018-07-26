dbdaa05542ec27846077bb8104db7a7981566366
lilongsheng <1226364644@qq.com>
2018-07-26 23:29:37 +0800

    update function

--- code/function.php
+++ code/function.php
@@ -5,7 +5,19 @@
  * CreateTime: 2018-7-7 21:19:58
  * Description: 
  */
-function unsetCookie($cookie) {
-    setcookie($cookie, '', time() - 3600);
-    unset($_COOKIE[$cookie]);
+function clearCookie($cookie) {
+    setcookie($cookie, '', time() - 3600,'/');
+}
+
+function clearSession($session_key) {
+    //第一步
+    session_start();
+    //第二步
+    $_SESSION = array();
+    //第三步
+    if (isset($_COOKIE[session_name()])) {
+        setcookie(session_name(), '', time() - 3600,'/');
+    }
+    //第四步
+    session_destroy();
 }
