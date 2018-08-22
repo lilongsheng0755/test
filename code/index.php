2cb043150e05eadb6615d7c3c4a58b5a3c8f9dce
lilongsheng <1226364644@qq.com>
2018-08-22 23:54:33 +0800

    test mysqli

--- code/index.php
+++ code/index.php
@@ -5,4 +5,21 @@
  * CreateTime: 2018-6-21 21:43:31
  * Description: 
  */
-phpinfo();
+//phpinfo();
+$host = '192.168.0.102';
+$username = 'test';
+$passwd = 'test';
+$dbname = 'test';
+$port = 3306;
+$mysqli = new mysqli($host, $username, $passwd, $dbname);
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+$mysqli->begin_transaction(MYSQLI_TRANS_START_READ_ONLY);
+
+$mysqli->query("SELECT title FROM test");
+$mysqli->commit();
+
+$mysqli->close();
