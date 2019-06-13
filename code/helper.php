24ad7a16da2cd3b3b959b576606d925bb5dbd265
lilongsheng <1226364644@qq.com>
2019-06-13 16:10:06 +0800

    deleted: code/helper/HelperManagerDB.php

---
+++ code/helper.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Author: skylong
+ * CreateTime: 2019-6-13 14:11:34
+ * Description: 
+ */
+class helper {
+    //put your code here
+}
+
--- code/mysql.php
+++ code/mysql.php
@@ -2,7 +2,6 @@
 
 include_once './lib/SingleBase.php';
 include_once './helper/HelperBackupDB.php';
-include_once './helper/HelperException.php';
 /**
  * Author: skylong
  * CreateTime: 2019-6-11 13:35:50
@@ -12,7 +11,7 @@ $conn = new \mysqli('192.168.3.182', 'root', 'root', 'test', 3306);
 $conn->set_charset("utf8mb4");
 
 try{
-    HelperBackupDB::getInstance($conn)->doing('./','',['user_base_info'=>'mid','user_base_info2'=>'mid']);
+    HelperBackupDB::getInstance($conn)->doing();
 }catch(HelperException $e){
     echo $e->getMessage();
 }
